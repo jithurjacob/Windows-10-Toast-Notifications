2dc41d7a29aae4d888a8b4a122620b7ce5586c3c
Siddhanta <sid.choco@gmail.com>
2016-12-17 19:11:06 +0800

    Action configuration working

--- __init__.py
+++ __init__.py
@@ -20,12 +20,12 @@ from time import sleep
 # 3rd party modules
 from win32api import GetModuleHandle, PostQuitMessage
 from win32con import CW_USEDEFAULT, IMAGE_ICON, IDI_APPLICATION,\
-                     LR_DEFAULTSIZE, LR_LOADFROMFILE,\
-                     WM_DESTROY, WS_OVERLAPPED, WS_SYSMENU, WM_USER
+					 LR_DEFAULTSIZE, LR_LOADFROMFILE,\
+					 WM_DESTROY, WS_OVERLAPPED, WS_SYSMENU, WM_USER
 from win32gui import CreateWindow, DestroyWindow, LoadIcon, LoadImage,\
-                     NIF_ICON, NIF_INFO, NIF_MESSAGE, NIF_TIP,\
-                     NIM_ADD, NIM_DELETE, NIM_MODIFY,\
-                     RegisterClass, Shell_NotifyIcon, UpdateWindow, WNDCLASS
+					 NIF_ICON, NIF_INFO, NIF_MESSAGE, NIF_TIP,\
+					 NIM_ADD, NIM_DELETE, NIM_MODIFY,\
+					 RegisterClass, Shell_NotifyIcon, UpdateWindow, WNDCLASS
 
 # ############################################################################
 # ########### Classes ##############
@@ -33,74 +33,74 @@ from win32gui import CreateWindow, DestroyWindow, LoadIcon, LoadImage,\
 
 
 class WindowsBalloonTip:
-    """Create a Windows 10 notification balloon.
+	"""Create a Windows 10 notification balloon.
 
-    from: https://github.com/jithurjacob/Windows-10-Toast-Notifications
-    """
+	from: https://github.com/jithurjacob/Windows-10-Toast-Notifications
+	"""
 
-    def __init__(self):
-        """Initialize."""
-        message_map = {WM_DESTROY: self.on_destroy, }
+	def __init__(self):
+		"""Initialize."""
+		message_map = {WM_DESTROY: self.on_destroy, }
 
-        # Register the window class.
-        wc = WNDCLASS()
-        self.hinst = wc.hInstance = GetModuleHandle(None)
-        wc.lpszClassName = str("PythonTaskbar")  # must be a string
-        wc.lpfnWndProc = message_map  # could also specify a wndproc.
-        self.classAtom = RegisterClass(wc)		
+		# Register the window class.
+		wc = WNDCLASS()
+		self.hinst = wc.hInstance = GetModuleHandle(None)
+		wc.lpszClassName = str("PythonTaskbar")  # must be a string
+		wc.lpfnWndProc = message_map  # could also specify a wndproc.
+		self.classAtom = RegisterClass(wc)      
 
-    def balloon_tip(self, title="Notification", msg="Here comes the message",
-                    icon_path="plugins\Win10Notifications\python.ico", duration=5):
-        """Notification settings.
+	def balloon_tip(self, title="Notification", msg="Here comes the message",
+					icon_path="plugins\Win10Notifications\python.ico", duration=5):
+		"""Notification settings.
 
-        :title: notification title
-        :msg: notification message
-        :icon_path: path to the .ico file to custom notification
-        :duration: delay in seconds before notification self-destruction
-        """
-        style = WS_OVERLAPPED | WS_SYSMENU
-        self.hwnd = CreateWindow(self.classAtom, "Taskbar", style,
-                                 0, 0, CW_USEDEFAULT,
-                                 CW_USEDEFAULT,
-                                 0, 0, self.hinst, None)
-        UpdateWindow(self.hwnd)
+		:title: notification title
+		:msg: notification message
+		:icon_path: path to the .ico file to custom notification
+		:duration: delay in seconds before notification self-destruction
+		"""
+		style = WS_OVERLAPPED | WS_SYSMENU
+		self.hwnd = CreateWindow(self.classAtom, "Taskbar", style,
+								 0, 0, CW_USEDEFAULT,
+								 CW_USEDEFAULT,
+								 0, 0, self.hinst, None)
+		UpdateWindow(self.hwnd)
 
-        # icon
-        icon_path = path.abspath(icon_path)
-        icon_flags = LR_LOADFROMFILE | LR_DEFAULTSIZE
-        try:
-            hicon = LoadImage(self.hinst, icon_path,
-                              IMAGE_ICON, 0, 0, icon_flags)
-        except Exception as e:
-        	logging.error("Some trouble with the icon ({0}): {1}".format(icon_path, e))
-        	hicon = LoadIcon(0, IDI_APPLICATION)
+		# icon
+		icon_path = path.abspath(icon_path)
+		icon_flags = LR_LOADFROMFILE | LR_DEFAULTSIZE
+		try:
+			hicon = LoadImage(self.hinst, icon_path,
+							  IMAGE_ICON, 0, 0, icon_flags)
+		except Exception as e:
+			logging.error("Some trouble with the icon ({0}): {1}".format(icon_path, e))
+			hicon = LoadIcon(0, IDI_APPLICATION)
 
-        # Taskbar icon
-        flags = NIF_ICON | NIF_MESSAGE | NIF_TIP
-        nid = (self.hwnd, 0, flags, WM_USER + 20, hicon, "Tooltip")
-        Shell_NotifyIcon(NIM_ADD, nid)
-        Shell_NotifyIcon(NIM_MODIFY, (self.hwnd, 0, NIF_INFO,
-                                      WM_USER + 20,
-                                      hicon, "Balloon Tooltip", msg, 200,
-                                      title))
-        # take a rest then destroy
-        sleep(duration)
-        DestroyWindow(self.hwnd)
-        return None
+		# Taskbar icon
+		flags = NIF_ICON | NIF_MESSAGE | NIF_TIP
+		nid = (self.hwnd, 0, flags, WM_USER + 20, hicon, "Tooltip")
+		Shell_NotifyIcon(NIM_ADD, nid)
+		Shell_NotifyIcon(NIM_MODIFY, (self.hwnd, 0, NIF_INFO,
+									  WM_USER + 20,
+									  hicon, "Balloon Tooltip", msg, 200,
+									  title))
+		# take a rest then destroy
+		sleep(duration)
+		DestroyWindow(self.hwnd)
+		return None
 
-    def on_destroy(self, hwnd, msg, wparam, lparam):
-        """Clean after notification ended.
+	def on_destroy(self, hwnd, msg, wparam, lparam):
+		"""Clean after notification ended.
 
-        :hwnd:
-        :msg:
-        :wparam:
-        :lparam:
-        """
-        nid = (self.hwnd, 0)
-        Shell_NotifyIcon(NIM_DELETE, nid)
-        PostQuitMessage(0)
+		:hwnd:
+		:msg:
+		:wparam:
+		:lparam:
+		"""
+		nid = (self.hwnd, 0)
+		Shell_NotifyIcon(NIM_DELETE, nid)
+		PostQuitMessage(0)
 
-        return None
+		return None
 
 
 class WindowsBalloonTip_EG(eg.PluginBase):
@@ -117,18 +117,21 @@ class ShowNotification(eg.ActionBase):
 	def __call__(self):
 		self.winballoon.balloon_tip(title="Title", msg="msg")
 
-	def Configure(self, myString=""):
+	def Configure(self, myString=""):		
+
 		panel = eg.ConfigPanel()
+
+		stText = wx.StaticText(panel, label="Message: ")
 		editTextCtrl = wx.TextCtrl(panel, -1, myString)
-		panel.sizer.Add(editTextCtrl, 1, wx.EXPAND)
-
-
-		sizer = wx.GridBagSizer(3,0)
-		expand = wx.EXPAND
-		align = wx.ALIGN_CENTER_VERTICAL
+		
+		sizer = wx.GridBagSizer(5,5)
 		sizer.AddMany([
-			(panel.StaticText("Message to display"), (0, 0), (1, 1), align),
-			(editTextCtrl, (1, 0), (1, 1), expand)])
+			(stText, (0,0), (1,1), wx.ALIGN_CENTER_VERTICAL),
+			(editTextCtrl, (0,1), (1,4), wx.ALIGN_CENTER_VERTICAL|wx.EXPAND)
+		])
+
+		sizer.AddGrowableCol(2)
+		panel.sizer.Add(sizer, 1, wx.EXPAND)
 
 		while panel.Affirmed():
 			panel.SetResult(editTextCtrl.GetValue())
