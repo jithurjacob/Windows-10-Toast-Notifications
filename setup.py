d4091fe67b086fde50ace54ecf7c8624da5916ce
Jithu R Jacob <jithurjacob@gmail.com>
2017-02-15 23:08:41 +0530

    added version 3.0

--- setup.py
+++ setup.py
@@ -20,12 +20,12 @@ requirements_txt = list(map(str, map(
 
 setup(
     name="win10toast",
-    version="0.2",
+    version="0.3",
     install_requires=requirements_txt,
     packages=["win10toast"],
     license="BSD",
     url="https://github.com/jithurjacob/Windows-10-Toast-Notifications",
-    download_url = 'https://github.com/jithurjacob/Windows-10-Toast-Notifications/tarball/0.1',
+    download_url = 'https://github.com/jithurjacob/Windows-10-Toast-Notifications/tarball/0.3',
     description=(
         "An easy-to-use Python library for displaying "
         "Windows 10 Toast Notifications"
