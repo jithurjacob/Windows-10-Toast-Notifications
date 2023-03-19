7c69cd1b7db656990ace0008481178aa16b54afa
Maurito master <mau.ignacioborre@gmail.com>
2023-03-18 22:30:11 -0300

    updated import locations for pip v.23, fixed attrgetter input

--- setup.py
+++ setup.py
@@ -1,22 +1,20 @@
 
 from operator import attrgetter
 from os import path
+from setuptools import setup
 
-#from pip.req import parse_requirements
 try:
     # pip >=20
-    #from pip._internal.network.session import PipSession
     from pip._internal.req import parse_requirements
 except ImportError:
     try:
         # 10.0.0 <= pip <= 19.3.1
-        #from pip._internal.download import PipSession
         from pip._internal.req import parse_requirements
     except ImportError:
         # pip <= 9.0.3
-        #from pip.download import PipSession
         from pip.req import parse_requirements
-from setuptools import setup
+
+
 
 def read(fname):
     return open(path.join(path.dirname(__file__), fname)).read()
